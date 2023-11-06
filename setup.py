41e60283ee8cdf4da8b458efab0b817d6cb524fc
vrbt <93156279+vrbt@users.noreply.github.com>
2023-11-06 05:32:39 +0000

    feat: fix setup.py
    added long_decription to setup.py for pypi

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='datasetter',
     version='0.1.0',
+    long_description_content_type='text/markdown',
+    long_description=open('README.md').read(),
     packages=find_packages(),
     install_requires=[],
     entry_points={
