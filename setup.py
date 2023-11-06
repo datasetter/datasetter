5fad553f05c815c26bf05ff3b53d5bba07b05dbb
vrbt <93156279+vrbt@users.noreply.github.com>
2023-11-06 05:46:46 +0000

    feat: update version
    updated version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='datasetter',
-    version='0.1.1',
+    version='0.1.2',
     long_description_content_type='text/markdown',
     long_description=open('README.md').read(),
     packages=find_packages(),
